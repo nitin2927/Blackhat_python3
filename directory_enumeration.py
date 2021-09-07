2df83e4aabd14089b1ff068b39f86c41f503982e
nitin2927 <75536852+nitin2927@users.noreply.github.com>
2021-09-07 20:55:31 +0530

    Update directory_enumeration.py

--- directory_enumeration.py
+++ directory_enumeration.py
@@ -1,1 +1,13 @@
+import requests
+import sys
 
+sub_list = open("wordlist.txt").read()
+directories = sub_list.splitlinen()
+
+for dir in directories:
+  dir_enim = f"http://{sys.argv[1]}/{dir}.html"
+  r = request.get(dir_enum)
+  if r.status_code==404:
+    pass
+  else:
+    print("Valid directory:" ,dir_enum)
